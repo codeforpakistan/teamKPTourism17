03bd1b410266022b4058b365f1e9012b8658c9bb
zartasha <sane_z9334@yahoo.com>
2017-04-18 10:01:12 +0500

    third page

---
+++ css/discover.php
@@ -0,0 +1,150 @@
+<?php
+?>
+<html>
+<head>
+<title>TCKP | Discover</title>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+<meta charset="utf-8">
+<link rel="stylesheet" id="bootstrap-css" href="./css/bootstrap.css" type="text/css" media="all">
+<link rel="stylesheet" id="style-css" href="./css/style.css" type="text/css" media="all">
+<link rel="stylesheet" id="animate-css" href="./css/animate.css" type="text/css" media="all">
+
+</head>
+
+
+<body class="pattern animated zoomIn">
+	<div class="container">	
+		<!-- TOP LOGO DIV -->	
+		<div class="row margins">
+			<div class="col-sm-4"></div>
+			<div class="col-sm-4 text-center margins">
+				<img src="images/logo.png" alt="">
+				<h2 class="color-blue">DISCOVER KP</h2>
+				<h4 class="color-blue">Choose a region to explore</h4>
+				<p class="color-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. </p>
+			</div>
+			<div class="col-sm-4"></div>
+		</div>
+
+		<!-- FAMOUS REGIONS DIV -->
+		<div class="col-lg-12 margins padding">
+				<div class="col-lg-4 col-sm-6 col-xs-12 text-center famous_regions_div swat zoom">
+					<div class="has-feedback">
+						<h2 id="swat" class="color-blue">Swat District</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+						<a href="details.php"><button class="btn btn-primary">EXPLORE</button></a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 col-xs-12 text-center famous_regions_div naran zoom">
+					<div class="has-feedback">
+						<h2 id="naran" class="color-blue">Naran</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+						<a href="#"><button class="btn btn-primary">EXPLORE</button></a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 col-xs-12 text-center famous_regions_div chitral zoom">
+					<div class="has-feedback">
+						<h2 id="chitral" class="color-blue">Chitral</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+						<a href="#"><button class="btn btn-primary">EXPLORE</button></a>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-sm-6 col-xs-12 text-center famous_regions_div abottabad zoom">
+					<div class="has-feedback">
+						<h2 id="abottabad" class="color-blue">Abbottabad</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+						<a href="#"><button class="btn btn-primary">EXPLORE</button></a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 col-xs-12 text-center famous_regions_div kalam zoom">
+					<div class="has-feedback">
+						<h2 id="kalam" class="color-blue">Kalam</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+						<a href="#"><button class="btn btn-primary">EXPLORE</button></a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 col-xs-12 text-center famous_regions_div place zoom">
+					<div class="has-feedback">
+						<h2 id="place" class="color-blue">Swat District</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+						<a href="#"><button class="btn btn-primary">EXPLORE</button></a>
+					</div>
+				</div>
+		</div>
+	</div>
+
+	<!-- OTHER REGIONS DIV -->
+	<div class="col-xs-12 white_bg other_regions">
+		<div class="row margins">
+			<h3 class="color-blue text-center">Other Regions</h3>
+			<div class="col-sm-8 right-separation">
+				<div class="col-sm-2"></div>
+				<div class="col-sm-10">
+					<h4 class="color-blue north_heading_margin">Northern KP</h4>
+					<div class="col-sm-6 small-screen-margin">
+						<ul>
+							<li class="other_regions_link"><a href="#">Home</a></li>
+							<li class="other_regions_link"><a href="#">Terms</a></li>
+							<li class="other_regions_link"><a href="#">Privacy Policy</a></li>
+							<li class="other_regions_link"><a href="#">Site Map</a></li>
+							<li class="other_regions_link"><a href="#">Discover Places</a></li>
+							<li class="other_regions_link"><a href="#">Navigation</a></li>
+						</ul>
+					</div>
+					<div class="col-sm-6 small-screen-margin">
+						<ul>
+							<li class="other_regions_link"><a href="#">Home</a></li>
+							<li class="other_regions_link"><a href="#">Terms</a></li>
+							<li class="other_regions_link"><a href="#">Privacy Policy</a></li>
+							<li class="other_regions_link"><a href="#">Site Map</a></li>
+							<li class="other_regions_link"><a href="#">Discover Places</a></li>
+							<li class="other_regions_link"><a href="#">Navigation</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-1 left-separation"></div>
+			<div class="col-sm-3">
+				<div class="col-sm-12 small-screen-margin">
+					<h4 class="color-blue">Southern KP</h4>
+					<ul>
+						<li class="other_regions_link"><a href="#">Home</a></li>
+						<li class="other_regions_link"><a href="#">Terms</a></li>
+						<li class="other_regions_link"><a href="#">Privacy Policy</a></li>
+						<li class="other_regions_link"><a href="#">Site Map</a></li>
+						<li class="other_regions_link"><a href="#">Discover Places</a></li>
+						<li class="other_regions_link"><a href="#">Navigation</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+	<!-- FOOTER DIV -->
+	<div class="footer col-xs-12">
+		<div class="col-sm-2"></div>
+		<div class="col-sm-8 text-center">
+			<ul>
+				<li class="footer-links"><a href="#">Home</a></li>
+				<li class="footer-links"><a href="#">Terms</a></li>
+				<li class="footer-links"><a href="#">Privacy Policy</a></li>
+				<li class="footer-links"><a href="#">Site Map</a></li>
+				<li class="footer-links"><a href="#">Discover Places</a></li>
+				<li class="footer-links"><a href="#">Navigation</a></li>
+			</ul>
+		</div>
+		<div class="col-sm-2"></div>
+	</div>
+
+</body>
+
+<script type="text/javascript" src="./js/jquery.min.js" ></script>
+<script type="text/javascript" src="./js/jquery.min.js" ></script>
+
+
+</html>
+
+		
+			
